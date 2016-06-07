986e69b8e3341b8ab6251c9854cdf2ac9321ad2e
Casey Flynn <caseyflynn@me.com>
2016-06-07 18:08:01 -0400

    Tetromino randomization tweak.

--- frontend/js/modules/tetromino-factory.js
+++ frontend/js/modules/tetromino-factory.js
@@ -11,10 +11,34 @@ Tetris.TetrominoFactory = function () {
     Tetris.Tetrominos.Z,
     Tetris.Tetrominos.I,
   ];
+
+  this.bag = [];
 };
 
 Tetris.TetrominoFactory.prototype.getRandom = function () {
-  var tetromino = new Tetris.Tetromino(this.tetrominos[Math.floor(Math.random() * this.tetrominos.length)]);
+  if (this.isEmpty()) {
+    this.fillBag();
+  }
+  var tetromino = new Tetris.Tetromino(this.bag.pop());
 
   return tetromino;
+};
+
+Tetris.TetrominoFactory.prototype.isEmpty = function () {
+  return this.bag.length === 0;
+};
+
+Tetris.TetrominoFactory.prototype.fillBag = function () {
+  this.bag = this.tetrominos.slice(0);
+  this.shuffle(this.bag);
+};
+
+Tetris.TetrominoFactory.prototype.shuffle = function (array) {
+  var j, x, i;
+  for (i = array.length; i; i -= 1) {
+      j = Math.floor(Math.random() * i);
+      x = array[i - 1];
+      array[i - 1] = array[j];
+      array[j] = x;
+  }
 };
