88ab7da16128ad36c7c344006ec3c0f5336a442c
Casey Flynn <caseyflynn@me.com>
2016-06-08 18:14:49 -0400

    Added file.

---
+++ frontend/js/config/levels.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var Tetris = Tetris || {};
+
+Tetris.Config = Tetris.Config || {};
+
+Tetris.Config.LEVELS = [
+  {
+    // Level 0
+    dropRate : 48,
+  },
+  {
+    // Level 1
+    dropRate : 43,
+  },
+  {
+    // Level 2
+    dropRate : 38,
+  },
+  {
+    // Level 3
+    dropRate : 33,
+  }
+];
